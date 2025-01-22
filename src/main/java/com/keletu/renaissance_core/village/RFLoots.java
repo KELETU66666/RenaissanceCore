16ade23b5b72caa49a1bfa5a559d2d56e51cb0d8
a <EYCHFSJD@gmail.com>
2025-01-22 17:02:43 +0800

    remove two useless file

--- src/main/java/com/keletu/renaissance_core/village/RFLoots.java
+++
@@ -1,9 +0,0 @@
-package com.keletu.renaissance_core.village;
-
-import com.keletu.renaissance_core.RenaissanceCore;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootTableList;
-
-public class RFLoots {
-    public static final ResourceLocation TE_TOWER = LootTableList.register(new ResourceLocation(RenaissanceCore.MODID, "chests/te_village"));;
-}
