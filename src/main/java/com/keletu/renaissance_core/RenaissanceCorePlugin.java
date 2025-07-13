2528d838f5b5a7206e54ea2a6b7975cbcef7f0c5
a <EYCHFSJD@gmail.com>
2025-07-13 15:43:02 +0800

    fix mixins and version bump.

--- src/main/java/com/keletu/renaissance_core/RenaissanceCorePlugin.java
+++ src/main/java/com/keletu/renaissance_core/RenaissanceCorePlugin.java
@@ -3,14 +3,14 @@ package com.keletu.renaissance_core;
 import java.util.Map;
 import fermiumbooter.FermiumRegistryAPI;
 import net.minecraftforge.fml.common.Loader;
-import org.spongepowered.asm.launch.MixinBootstrap;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
+@IFMLLoadingPlugin.SortingIndex(1001)
+@IFMLLoadingPlugin.Name("RenaissanceCore")
 public class RenaissanceCorePlugin implements IFMLLoadingPlugin {
 
 	public RenaissanceCorePlugin() {
-		MixinBootstrap.init();
 		//False for Vanilla/Coremod mixins, true for regular mod mixins
 		FermiumRegistryAPI.enqueueMixin(true, "mixins.renaissance_core.json");
 		FermiumRegistryAPI.enqueueMixin(true, "mixins.rc_research_change.json", () -> !Loader.isModLoaded("oldresearch"));
